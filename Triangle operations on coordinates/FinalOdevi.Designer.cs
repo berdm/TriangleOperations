f082dbd985b5b57c4ef7fdd27fa435efe4568e93
Buket Erdem <46616338+berdm@users.noreply.github.com>
2022-06-20 21:11:32 +0300

    Add files via upload

---
+++ Triangle operations on coordinates/FinalOdevi.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace FinalOdevi
+{
+    partial class frmFinalOdevi
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEksenCiz = new System.Windows.Forms.Button();
+            this.btnNesneCiz = new System.Windows.Forms.Button();
+            this.btnNesneYansit = new System.Windows.Forms.Button();
+            this.btnDondur = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnEksenCiz
+            // 
+            this.btnEksenCiz.Location = new System.Drawing.Point(52, 117);
+            this.btnEksenCiz.Name = "btnEksenCiz";
+            this.btnEksenCiz.Size = new System.Drawing.Size(75, 41);
+            this.btnEksenCiz.TabIndex = 0;
+            this.btnEksenCiz.Text = "Eksenleri Çiz";
+            this.btnEksenCiz.UseVisualStyleBackColor = true;
+            this.btnEksenCiz.Click += new System.EventHandler(this.btnEksenCiz_Click);
+            // 
+            // btnNesneCiz
+            // 
+            this.btnNesneCiz.Location = new System.Drawing.Point(52, 176);
+            this.btnNesneCiz.Name = "btnNesneCiz";
+            this.btnNesneCiz.Size = new System.Drawing.Size(75, 41);
+            this.btnNesneCiz.TabIndex = 1;
+            this.btnNesneCiz.Text = "Nesneyi Çiz (Üçgen)";
+            this.btnNesneCiz.UseVisualStyleBackColor = true;
+            this.btnNesneCiz.Click += new System.EventHandler(this.btnNesneCiz_Click);
+            // 
+            // btnNesneYansit
+            // 
+            this.btnNesneYansit.Location = new System.Drawing.Point(52, 232);
+            this.btnNesneYansit.Name = "btnNesneYansit";
+            this.btnNesneYansit.Size = new System.Drawing.Size(75, 36);
+            this.btnNesneYansit.TabIndex = 2;
+            this.btnNesneYansit.Text = "Yansıt(X)";
+            this.btnNesneYansit.UseVisualStyleBackColor = true;
+            // 
+            // btnDondur
+            // 
+            this.btnDondur.Location = new System.Drawing.Point(52, 285);
+            this.btnDondur.Name = "btnDondur";
+            this.btnDondur.Size = new System.Drawing.Size(75, 39);
+            this.btnDondur.TabIndex = 3;
+            this.btnDondur.Text = "Döndür";
+            this.btnDondur.UseVisualStyleBackColor = true;
+            // 
+            // frmFinalOdevi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(799, 478);
+            this.Controls.Add(this.btnDondur);
+            this.Controls.Add(this.btnNesneYansit);
+            this.Controls.Add(this.btnNesneCiz);
+            this.Controls.Add(this.btnEksenCiz);
+            this.Name = "frmFinalOdevi";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Final Ödevi";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEksenCiz;
+        private System.Windows.Forms.Button btnNesneCiz;
+        private System.Windows.Forms.Button btnNesneYansit;
+        private System.Windows.Forms.Button btnDondur;
+    }
+}
+
---
+++ Triangle operations on coordinates/FinalOdevi.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FinalOdevi
+{
+    public partial class frmFinalOdevi : Form
+    {
+        System.Drawing.Graphics Cizim;
+        Graphics GrafikCiz;
+        Pen kalem = new Pen(Color.Purple, 3);
+        Point Nokta1, Nokta2, Nokta3;
+        //Pen kalem = new Pen(Color.Purple,2);
+        Double[,] Xcisim = new Double[3,2] { { 20, 30 }, { 70, 30 }, { 20, 60 } };
+        Point[] NoktalarPoints = new Point[3];
+
+        Double[,] Xnew = new Double[3, 2];
+
+       // Double[,] Tisometric = new Double[2, 2] 
+       /*
+        Double[,] Ttranslation = new Double[4, 4] { {1,0,0,0 },
+                                                  {0,1,0,0 },
+                                                   {0,0,1,0 },
+                                                      {0,0,0,1 }};
+       
+        Double[,] Trotate = new Double[4, 4] { {1,0,0,0 },
+                                                  {0,1,0,0 },
+                                                   {0,0,1,0 },
+                                                      {0,0,0,1 }};
+       */
+
+        Double[,] axis2BX = new Double[2, 2] { { -2.5, 0 }, { 2.5, 0 } };
+        Double[,] axis2BY = new Double[2, 2] { { 0, 2.5 }, { 0, -2.5 } };
+
+       // int i, j, k;
+
+        
+        public frmFinalOdevi()
+        {
+            InitializeComponent();
+        }
+        
+        private int CaCoordinateX(Double tempX)
+        {
+            return Convert.ToInt32(300 + 250 + (100 * tempX));
+        }
+
+        private int CaCoordinateY(Double tempY)
+        {
+            return Convert.ToInt32(100 + 250 + (-100 * tempY));
+        }
+        
+        private void btnNesneCiz_Click(object sender, EventArgs e)
+        {
+            GrafikCiz = this.CreateGraphics();
+            kalem.Width = 5;
+
+            Nokta1 = new Point(20, 30);
+            Nokta2 = new Point(70, 30);
+            Nokta3 = new Point(20, 60);
+
+            GrafikCiz.DrawLine(kalem, Nokta1.X, Nokta1.Y, Nokta2.X, Nokta2.Y);
+            GrafikCiz.DrawLine(kalem, Nokta2.X, Nokta2.Y, Nokta3.X, Nokta3.Y);
+
+            for( int i=0; i<3; i++)
+            {
+                NoktalarPoints[i].X = Convert.ToInt32(Xcisim[i, 0]);
+                NoktalarPoints[i].Y = Convert.ToInt32(Xcisim[i, 1]);
+            }
+            kalem.Color = Color.Cyan;
+            GrafikCiz.DrawPolygon(kalem, NoktalarPoints);
+
+        }
+        
+    
+
+        private void draw()
+        {
+            
+            Cizim = this.CreateGraphics();
+            Cizim.DrawLine(kalem, CaCoordinateX(Xnew[0, 0]), CaCoordinateY(Xnew[0, 1]), CaCoordinateX(Xnew[1, 0]), CaCoordinateY(Xnew[1, 1]));
+            Cizim.DrawLine(kalem, CaCoordinateX(Xnew[1, 0]), CaCoordinateY(Xnew[1, 1]), CaCoordinateX(Xnew[2, 0]), CaCoordinateY(Xnew[2, 1]));
+         
+
+        }
+
+        private void btnEksenCiz_Click(object sender, EventArgs e)
+        {
+            
+            Cizim = this.CreateGraphics();
+            Cizim.DrawLine(kalem, CaCoordinateX(axis2BX[0, 0]), CaCoordinateY(axis2BX[0, 1]), CaCoordinateX(axis2BX[1, 0]), CaCoordinateY(axis2BX[1, 1]));
+            Cizim.DrawLine(kalem, CaCoordinateX(axis2BY[0, 0]), CaCoordinateY(axis2BY[0, 1]), CaCoordinateX(axis2BY[1, 0]), CaCoordinateY(axis2BY[1, 1]));
+
+            Cizim.Dispose();
+           
+
+
+        }
+
+    }
+}
